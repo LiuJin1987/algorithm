a60da33995534e3760ee5eb8b1f275223e7e34ea
刘津 <liujinlc@inspur.com>
2021-04-22 15:32:12 +0800

    fix Ex3 of chapter1 section2 by introducing duplication removal

--- src/chapter1/section2/Ex3.java
+++ src/chapter1/section2/Ex3.java
@@ -1,11 +1,12 @@
 package chapter1.section2;
 
-import common.util.ArgsChecker;
 import edu.princeton.cs.algs4.Interval1D;
 import edu.princeton.cs.algs4.Interval2D;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.HashMap;
 
 /**
  * @author liujinlc
@@ -58,11 +59,20 @@ class Interval2DTest {
         }
     }
 
+    /**
+     * Get contains count from intervals
+     *
+     * There is no need to remove duplication because there is no
+     * "contains each-other" relationship unless the interval is its self
+     *
+     * @param intervals     Interval2DExtend array contains Interval2DExtends to get contains count
+     * @return contains count
+     */
     public static int getContainCount(Interval2DExtend[] intervals) {
         int containCount = 0;
         for (Interval2DExtend self : intervals) {
             for (Interval2DExtend other : intervals) {
-                if (!self.equals(other) && self.contains(other)) {
+                if (notSelf(self, other) && self.contains(other)) {
                     containCount++;
                 }
             }
@@ -70,16 +80,57 @@ class Interval2DTest {
         return containCount;
     }
 
+    /**
+     *  Get overlap count from intervals
+     *
+     *  Introduce a overlap map to remove duplicate overlap relationship
+     *  when two interval contains each other by overlapping each other's
+     *  one corner likes below:
+     *               ______________________
+     *              |                     |
+     *              |                     |
+     *  ____________|___________          |
+     *  |           |          |          |
+     *  |           |          |          |
+     *  |           |__________|__________|
+     *  |                      |
+     *  |                      |
+     *  ________________________
+     *
+     * @param intervals     Interval2DExtend array contains Interval2DExtends to get overlap count
+     * @return overlap count
+     */
     public static int getOverlapCount(Interval2DExtend[] intervals) {
-        int overlapCount = 0;
+
+        HashMap<Interval2DExtend, Interval2DExtend> overlapMap =
+                new HashMap<>(intervals.length / 3 * 4 + 1);
         for (Interval2DExtend self : intervals) {
             for (Interval2DExtend other : intervals) {
-                if (!self.equals(other) && self.overlaps(other)) {
-                    overlapCount++;
+                if (notSelf(self, other) && isOneWayOverlap(overlapMap, self, other)) {
+                    overlapMap.put(self, other);
                 }
             }
         }
-        return overlapCount;
+        return overlapMap.size();
+    }
+
+    private static boolean isOneWayOverlap(HashMap<Interval2DExtend, Interval2DExtend> overlapMap,
+                                           Interval2DExtend self,
+                                           Interval2DExtend other) {
+        return isOverlaps(self, other) && notRecorded(overlapMap, other);
+    }
+
+    private static boolean notRecorded(HashMap<Interval2DExtend, Interval2DExtend> overlapMap,
+                                       Interval2DExtend other) {
+        return !overlapMap.containsKey(other);
+    }
+
+    private static boolean isOverlaps(Interval2DExtend self, Interval2DExtend other) {
+        return self.overlaps(other);
+    }
+
+    private static boolean notSelf(Interval2DExtend self, Interval2DExtend other) {
+        return !self.equals(other);
     }
 
     public Interval2DExtend[] generateAll() {
@@ -87,6 +138,10 @@ class Interval2DTest {
         for (int i = 0; i < intervalNumber; i++) {
             intervals[i] = generateArea();
         }
+        /*Interval2DExtend[] intervals = new Interval2DExtend[3];
+        intervals[0] = new Interval2DExtend(new Interval1D(0.3, 0.5), new Interval1D(0.2, 0.5));
+        intervals[1] = new Interval2DExtend(new Interval1D(0.6, 0.7), new Interval1D(0.1, 0.4));
+        intervals[2] = new Interval2DExtend(new Interval1D(0.4, 0.8), new Interval1D(0.3, 0.6));*/
         return intervals;
     }
 
@@ -115,8 +170,18 @@ class Interval2DExtend {
         this.rightBottom = new Point2D(x.max(), y.min());
     }
 
-    public boolean equals(Interval2DExtend that) {
-        return this.interval2D.equals(that.interval2D);
+
+    @Override
+    public boolean equals(Object that) {
+        if (!(that instanceof Interval2DExtend)) {
+            return false;
+        }
+        return this.interval2D.equals(((Interval2DExtend)that).interval2D);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.interval2D.hashCode();
     }
 
     public void draw() {
